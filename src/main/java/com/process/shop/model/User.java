30c83b6e7ba48a5b52413657d02c7e8b24bb2e1d
CarlosMolina07 <camolinar@ufpso.edu.co>
2024-03-14 21:12:09 -0500

    Anotaciones y creación de objeto estatico

--- src/main/java/com/process/shop/model/User.java
+++ src/main/java/com/process/shop/model/User.java
@@ -1,18 +1,20 @@
 package com.process.shop.model;
 
+import com.process.shop.model.enums.DocumentType;
+import lombok.Builder;
 import lombok.Data;
-
 import java.util.Date;
+import java.util.List;
 
 @Data
+@Builder
 public class User {
     private String fullName;
     private Date birthDay;
-    private String identificationType;
-    private String identification;
+    private DocumentType documentType;
+    private String document;
     private String phoneNumber;
     private String email;
     private String password;
-    private String address;
-
+    private List<Address> address;
 }
--- src/main/java/com/process/shop/service/UserServiceImpl.java
+++ src/main/java/com/process/shop/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.process.shop.service;
 
+import com.process.shop.model.Address;
 import com.process.shop.model.User;
+import com.process.shop.model.enums.DocumentType;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.List;
-
+@Service
 public class UserServiceImpl implements UserService{
-
     @Override
     public User createUser(User user) {
         return null;
@@ -17,7 +21,23 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUserById(Long id) {
-        return new User();
+        return User.builder()
+                .fullName("Pepito Perez")
+                .document("123")
+                .documentType(DocumentType.CC)
+                .address(List.of(
+                        Address.builder()
+                        .street("circunvalar")
+                        .avenue("1212")
+                        .neighborhood("12345")
+                        .postalCode("203040")
+                        .build()))
+                .password("admin")
+                .phoneNumber("3125487")
+                .birthDay(new Date())
+                .email("mail@email.com")
+                .build();
+
     }
 
     @Override
