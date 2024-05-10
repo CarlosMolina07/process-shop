be9002fa5f5cd2bcf462c9a4bad31af57a865931
CarlosMolina07 <camolinar@ufpso.edu.co>
2024-05-09 20:55:15 -0500

    Agregando validaciones al CRUD

--- src/main/java/com/process/shop/model/User.java
+++ src/main/java/com/process/shop/model/User.java
@@ -2,6 +2,10 @@ package com.process.shop.model;
 
 import com.process.shop.model.enums.DocumentType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,12 +21,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Please provide the full name")
+    @Size(min = 10, max = 200)
     private String fullName;
-    private LocalDate birthDay;
-    private String documentType;
+    @NotBlank(message = "Please provide the document")
+    @Size(min = 5, max = 10)
     private String document;
+    @NotBlank(message = "Please provide the document type")
+    private String documentType;
+    private LocalDate birthDay;
     private String phoneNumber;
+    @NotBlank(message = "Please provide the email")
+    @Email(message = "Please provide the email valid")
     private String email;
+    @NotBlank(message = "Please provide the password")
+    @Size(min = 8, max = 15)
     private String password;
     @OneToMany(mappedBy = "user")
     private List<Address> address;
